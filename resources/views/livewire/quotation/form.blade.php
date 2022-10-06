4ae76094ac2ea05fcae165d71db002b13ba6e60a
Ridho Al Amzah <58523131+ridho15@users.noreply.github.com>
2022-10-06 23:09:47 +0700

    Update pembaharuan

--- resources/views/livewire/quotation/form.blade.php
+++ resources/views/livewire/quotation/form.blade.php
@@ -22,7 +22,7 @@
                             <label for="" class="form-label required">Status Response</label>
                             <select name="status_response" class="form-select form-select-solid" wire:model="status_response" data-control="select2" data-dropdown-parent="#modal_form" data-placeholder="Pilih" required>
                                 <option value="">Pilih</option>
-                                @foreach ($listStatusBarang as $item)
+                                @foreach ($listStatusResponse as $item)
                                     <option value="{{ $item['status_response'] }}">{{ $item['keterangan'] }}</option>
                                 @endforeach
                             </select>
@@ -34,8 +34,8 @@
                             <label for="" class="form-label required">Tipe Pembayaran</label>
                             <select name="id_tipe_pembayaran" class="form-select form-select-solid" wire:model="id_tipe_pembayaran" data-control="select2" data-dropdown-parent="#modal_form" data-placeholder="Pilih" required>
                                 <option value="">Pilih</option>
-                                @foreach ($listStatusBarang as $item)
-                                    <option value="{{ $item['id_tipe_pembayaran'] }}">{{ $item['keterangan'] }}</option>
+                                @foreach ($listTipePembayaran as $item)
+                                    <option value="{{ $item->id }}">{{ $item->nama_tipe }}</option>
                                 @endforeach
                             </select>
                             @error('id_tipe_pembayaran')
