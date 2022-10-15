b1b122bb5f609492e4bb71b95d5fcdfe080cd9a4
dhaydt <1992dayat@gmail.com>
2022-10-15 16:56:35 +0700

    fix config asset

--- config/app.php
+++ config/app.php
@@ -1,5 +1,11 @@
 <?php
-
+if(env('APP_SET') == 'dayat'){
+    $asset = 'http://erp.test';
+}elseif(env('APP_SET') == 'server'){
+    $asset = 'http://erp.howsit-web.com';
+}else{
+    $asset = 'http://erp_deploy.test';
+}
 return [
 
     /*
@@ -54,7 +60,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', 'http://erp_deploy.test'),
+    'asset_url' => $asset,
 
     /*
     |--------------------------------------------------------------------------
