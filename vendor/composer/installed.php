6c8f1905997e2b41d0803fb5130b2cc696b98520
Ridho Al Amzah <58523131+ridho15@users.noreply.github.com>
2022-10-20 01:25:31 +0700

    Update Conflict

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -463,11 +463,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-<<<<<<< HEAD
             'reference' => 'ad1d206d4e1986212aba91f5cc15b30729c1b475',
-=======
-            'reference' => '062d2ef15c47b734847ea707fad24fa92d198642',
->>>>>>> ca0fd2901570274e090fdf917c9707d54557b5d5
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
