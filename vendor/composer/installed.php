49791cc68f78d7b74e9868f8e89d478dc22b8fe2
Ridho Al Amzah <ralamzah@gmail.com>
2022-12-09 16:52:46 +0700

    Update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b36a21bc4b7bb406fc9db89b1127be3f1dac51cc',
+        'reference' => 'c10a006b0ddc8dadbd429c657abdd2ecc0c62bb8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -460,7 +460,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b36a21bc4b7bb406fc9db89b1127be3f1dac51cc',
+            'reference' => 'c10a006b0ddc8dadbd429c657abdd2ecc0c62bb8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
