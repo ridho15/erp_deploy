fcf5b3822c5384f7674fa5b9beaf806269d53be9
Ridho Al Amzah <58523131+ridho15@users.noreply.github.com>
2023-03-27 13:35:20 +0700

    Update

--- app/Http/Controllers/ManagementTugasController.php
+++ app/Http/Controllers/ManagementTugasController.php
@@ -47,9 +47,9 @@ class ManagementTugasController extends Controller
         $data['web_logo'] = WebConfig::where('type', 'logo')->first();
         $data['web_name'] = WebConfig::where('type', 'web_name')->first();
         $data['listCatatanTeknisiPekerjaan'] = CatatanTeknisiPekerjaan::where('id_laporan_pekerjaan', $id)->get();
-        $pdf = Pdf::loadView('pdf_view.laporan_pekerjaan', $data);
+        // $pdf = Pdf::loadView('pdf_view.laporan_pekerjaan', $data);
 
-        return $pdf->download('laporan_pekerjaan_'.strtotime(now()).'.pdf');
+        // return $pdf->download('laporan_pekerjaan_'.strtotime(now()).'.pdf');
 
         return view('pdf_view.laporan_pekerjaan', $data);
     }
