cdb881c350955c17bd6ebdfc100a0fd78a89779e
Ridho Al Amzah <58523131+ridho15@users.noreply.github.com>
2023-04-04 20:55:33 +0700

    Update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Kostumer extends Model
+class Customer extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table = 'customers';
